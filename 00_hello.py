3035b13ca67803f4735470bd1f336576e6442fe8
= <=>
2023-09-23 15:00:28 -0500

    Primer Commit

---
+++ 00_hello.py
@@ -0,0 +1,24 @@
+#Esto es un Comentario :
+#Nuestro Hola-Mundo de Python
+'''
+Comentario Multilinea
+es decir en 
+varias lìneas
+'''
+#Imprimir en Consola
+print('Hola Python')
+print("Hola Python")
+print (4+9)
+
+#Consultar el tipo de Dato
+print(type("Soy Un dato String")) #Tipo Str
+print(type(5)) #Tipo Int
+print(type(1.5)) #Tipo Float
+print(type(True)) #Tipo Boolean
+
+#Variables
+
+
+
+
+
---
+++ 01_Variables.py
@@ -0,0 +1,6 @@
+#Variables
+my_variable = "My String Variable"
+last_name = "Flores Zaher Morales"
+
+print(my_variable)
+print(last_name)
